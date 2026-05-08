e9cabcceb5d1f5952967cc3f93ead37cb4104950
shreyanshu005 <shreyanshu005@gmail.com>
2026-05-08 23:14:31 +0530

    Initial Commit

---
+++ Aero/AeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroApp.swift
+//  Aero
+//
+//  Created by Shreyanshu on 08/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroTests/AeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  AeroTests.swift
+//  AeroTests
+//
+//  Created by Shreyanshu on 08/05/26.
+//
+
+import Testing
+@testable import Aero
+
+struct AeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
